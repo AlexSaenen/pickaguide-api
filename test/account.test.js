da2ebe7f2066f7024d06748889db3e47eb59290b
P-Hneutre <ph.gousse@gmail.com>
2017-01-29 21:13:12 +0000

    test resend-email

--- test/account.test.js
+++ test/account.test.js
@@ -2,8 +2,10 @@
 
 const request = require('supertest');
 
+const expect = require('chai').expect;
 const server = require('../index');
 const helpers = require('./helpers');
+const nock = require('nock');
 
 describe('Account Routes', () => {
   let app, user;
@@ -41,6 +43,34 @@ describe('Account Routes', () => {
         .set('Authorization', 'Bearer ' + user.account.token)
         .expect(200, done);
     });
+  });
+  
+  describe('GET /account/:id/resend-email', () => {
+    
+    it('should return error if email fail to send', (done) => {
+      let body;
+      let emailSent = nock('https://api.mailgun.net/v3/mg.pickaguide.fr')
+        .post(/messages/, function (b) {
+          body = b;
+          return true;
+        })
+        .reply(200, {status: 'sent'});
+  
+      request(app)
+        .get('/account/' + user._id + '/resend-email')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', 'Bearer ' + user.account.token)
+        .expect(200, (err, res) => {
+          if (err) done(err);
+          expect(body.from).to.be.eql('equipe@pickaguide.fr');
+          expect(body.to).to.be.eql('test@test.test');
+          expect(res.body.code).to.be.equal(0);
+          expect(res.body.message).to.be.eql('Confirmation email has been resent');
+          expect(emailSent.isDone()).to.be.true;
+          done();
+        });
+    });
+    
   });
 
 });
