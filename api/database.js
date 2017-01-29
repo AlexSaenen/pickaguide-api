397e4f3ad2e509f10d18f85e55f4833c6660168a
P-Hneutre <ph.gousse@gmail.com>
2017-01-29 16:04:35 +0000

    try to fix circle tests fail

--- api/database.js
+++ api/database.js
@@ -5,7 +5,9 @@ const config = require('config');
 
 mongoose.Promise = global.Promise;
 
-const init = () => mongoose.connect(config.mongo.url);
+const init = () => {
+  return mongoose.connect(config.mongo.url);
+};
 
 exports.ObjectId = mongoose.Types.ObjectId;
 
