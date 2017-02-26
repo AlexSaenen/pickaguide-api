cf7ee80297f8dd21c2dde4cf867b3b88065e2878
P-Hneutre <ph.gousse@gmail.com>
2017-02-26 12:54:27 +0000

    draft upload images via multer

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const profileHandler = require('../handlers/profile').Profile;
+const multer = require('multer');
 
 const router = express.Router();
+const upload = multer({ dest: __dirname + '/../../public/uploads/' });
 
 router.get('/:id', (req, res) => {
   profileHandler.find(req.params.id)
@@ -13,6 +15,11 @@ router.put('/', (req, res) => {
   profileHandler.update(req.user.userId, { profile: req.body })
     .then(result => res.status(200).send({ profile: result.profile }))
     .catch(error => res.status(400).send(error));
+});
+
+router.post('/upload/profile', upload.single('profile'), (req, res) => {
+  console.log(req)
+  return;
 });
 
 module.exports = router;
