a2dfe25ee19b9486db3fbc673d707d80809a6c66
P-Hneutre <ph.gousse@gmail.com>
2017-03-07 19:39:15 +0000

    upload without gridFS

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const profileHandler = require('../handlers/profile').Profile;
 const multer = require('multer');
+var mkdirp = require('mkdirp');
+const fs = require('fs');
 
 const router = express.Router();
-const upload = multer({ dest: __dirname + '/../../public/uploads/' });
+
+const storage = multer.diskStorage({
+  destination: function (req, file, next) {
+    console.log(req)
+    console.log(file)
+    mkdirp(__dirname + '/../../assets/' + req.user.userId + '/', function (err) {
+      if (err) next(err);
+      next(null, __dirname + '/../../assets/' + req.user.userId + '/')
+    });
+  },
+  filename: function (req, file, next) {
+    let extArray = file.mimetype.split("/");
+    let extension = extArray[extArray.length - 1];
+    next(null, file.fieldname + '.' + extension)
+  }
+});
+
+const upload = multer({ storage: storage});
 
 router.get('/:id', (req, res) => {
   profileHandler.find(req.params.id)
@@ -17,9 +36,32 @@ router.put('/', (req, res) => {
     .catch(error => res.status(400).send(error));
 });
 
-router.post('/upload/profile', upload.single('profile'), (req, res) => {
-  console.log(req)
-  return;
+router.post('/avatar', upload.single('avatar'), (req, res) => {
+  console.log(req);
+  profileHandler.update(req.user.userId, { profile: { photoUrl: req.file.filename } })
+    .then(result => res.sendStatus(201))
+    .catch(error => res.status(404).send(error));
+});
+
+router.get('/:id/avatar', (req, res, next) => {
+  
+  var options = {
+    root: __dirname + '/../../assets/' + req.params.id + '/',
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  };
+  
+  res.sendFile('avatar.jpeg', options, function (err) {
+    if (err) {
+      console.log(err);
+      next(err);
+    } else {
+      console.log('Sent:');
+    }
+  });
 });
 
 module.exports = router;
