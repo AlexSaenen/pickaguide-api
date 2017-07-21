428e474a861c45134907c582b17fb4867939e705
AlexSaenen <alexander.saenen@epitech.eu>
2017-07-21 14:25:20 +0200

    Clean payment-service

--- api/payment-service.js
+++ api/payment-service.js
@@ -1,7 +1,7 @@
 'use strict';
-//import stripePackage from 'stripe';
+
 const stripePackage = require('stripe');
-const userHandler = require('./handlers/user').User;
+// const userHandler = require('./handlers/user').User;
 
 const stripe = stripePackage('sk_test_LsvsnxVyCIQplUZ7oZioWvcL');
 
@@ -38,81 +38,78 @@ const stripe = stripePackage('sk_test_LsvsnxVyCIQplUZ7oZioWvcL');
 
 exports.createUser = (user) => {
   return new Promise((resolve, reject) => {
-    stripe.customers.create({
-      email: user.account.email,
-    }).then(function(customer) {
-      user.account.paymentId = customer.id;
-      user.account.save((saveErr, updatedUser) => {
-        if (saveErr) {
-          return reject(saveErr);
-        }
-        resolve(customer);
-      });
-    }).catch(function(err) {
-      reject(err);
-    })
+    stripe.customers
+      .create({
+        email: user.account.email,
+      })
+      .then((customer) => {
+        user.account.paymentId = customer.id;
+
+        user.account.save((saveErr) => {
+          if (saveErr) {
+            return reject(saveErr);
+          }
+
+          resolve(customer);
+        });
+      })
+      .catch(err => reject(err));
   });
-}
+};
 
 exports.getUser = (user) => {
   return new Promise((resolve, reject) => {
-    stripe.customers.retrieve(
-      user.account.paymentId
-    ).then(function(customer) {
-      resolve(customer);
-    }).catch(function(err) {
-      reject(err);
-    })
+    stripe.customers
+      .retrieve(user.account.paymentId)
+      .then(customer => resolve(customer))
+      .catch(err => reject(err));
   });
-}
+};
 
-exports.addCard = (idUser, body) => {// exp_month, exp_year, number, cvc) => {
-    return new Promise((resolve, reject) => {
-    stripe.customers.createSource(idUser, {
-      source: {
-         object: 'card',
-         exp_month: body.exp_month,
-         exp_year: body.exp_year,
-         number: body.number,
-         cvc: body.cvc
-      }
-    }).then(function(card) {
-      resolve(card);
-    }).catch(function(err) {
-      reject(err);
-    })
-  });
-}
-
-exports.createPayment = (idUser, body) => {// idCard, amount, currency, description) => {
+exports.addCard = (idUser, body) => { // exp_month, exp_year, number, cvc) => {
   return new Promise((resolve, reject) => {
-    stripe.charges.create({
-      amount: body.amount * 100,
-      currency: body.currency,
-      customer: idUser,
-      source: body.idCard,
-      description: body.description,
-      metadata: body.meta
-    }).then(function(payment) {
-      resolve(payment);
-    }).catch(function(err) {
-      reject(err);
-    })
+    stripe.customers
+      .createSource(idUser, {
+        source: {
+          object: 'card',
+          exp_month: body.exp_month,
+          exp_year: body.exp_year,
+          number: body.number,
+          cvc: body.cvc,
+        },
+      })
+      .then(card => resolve(card))
+      .catch(err => reject(err));
   });
-}
+};
+
+exports.createPayment = (idUser, body) => { // idCard, amount, currency, description) => {
+  return new Promise((resolve, reject) => {
+    stripe.charges
+      .create({
+        amount: body.amount * 100,
+        currency: body.currency,
+        customer: idUser,
+        source: body.idCard,
+        description: body.description,
+        metadata: body.meta,
+      })
+      .then(payment => resolve(payment))
+      .catch(err => reject(err));
+  });
+};
 
 exports.listPaymentFromUser = (idUser) => {
   return new Promise((resolve, reject) => {
-    stripe.charges.list({
-      customer: idUser,
-      limit: 100
-    }).then(function(payments) {
-      resolve(payments);
-    }).catch(function(err) {
-      reject(err);
-    })
-  })
-}
+    stripe.charges
+      .list({
+        customer: idUser,
+        limit: 100,
+      })
+      .then(payments => resolve(payments))
+      .catch(err => reject(err));
+  });
+};
 
 // exports.createCard = (userId, exp_month, exp_year, number, cvc) => {
 //   return new Promise((resolve, reject) => {
