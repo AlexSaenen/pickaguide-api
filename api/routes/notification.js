72a588cacca15f52df8ef682699fd06cb3895131
AlexSaenen <alexander.saenen@epitech.eu>
2017-09-11 09:01:17 +0200

    Create initial codebase for notifications

---
+++ api/routes/notification.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const notificationHandler = require('../handlers/notification').Notification;
+
+const router = express.Router();
+
+// TODO with infiniteScroll
+router.get('/', (req, res) => {
+  notificationHandler.findAllFrom(req.user.userId)
+    .then(result => res.status(200).send(result))
+    .catch(error => res.status(400).send(error));
+});
+
+router.get('/hasUnread', (req, res) => {
+  notificationHandler.hasUnread(req.user.userId)
+    .then(result => res.status(200).send(result))
+    .catch(error => res.status(400).send(error));
+});
+
+router.put('/read', (req, res) => {
+  notificationHandler.readAll(req.user.userId)
+    .then(result => res.status(200).send(result))
+    .catch(error => res.status(400).send(error));
+});
+
+router.put('/:id/read', (req, res) => {
+  notificationHandler.read(req.params.id, req.user.userId)
+    .then(result => res.status(200).send(result))
+    .catch(error => res.status(500).send(error));
+});
+
+module.exports = router;
